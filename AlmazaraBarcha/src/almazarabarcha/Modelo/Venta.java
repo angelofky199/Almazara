35953a1cd43b0c08264cfa6873e4d61fcc66cf15
Jose Ferrús <jose.ferrus.94@gmail.com>
2016-07-22 12:54:45 +0200

    commitAseao

--- AlmazaraBarcha/src/almazarabarcha/Modelo/Venta.java
+++ AlmazaraBarcha/src/almazarabarcha/Modelo/Venta.java
@@ -1,29 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package almazarabarcha.Modelo;
 
-import java.util.Date;
 
 /**
  *
  * @author jose
  */
 public class Venta {
-    private Date fecha;             //*
-    private String nombre_cliente;  //*
-    private String dni;
-    private String direccion;
-    private String poblacion;
-    private String telefono;
-    
     private static final String[] TIPOS_ACEITES = {"Lampante","Extra","Arberquina", "Bote Flor", "Bote Extra"};
     private static final String[] TIPOS_ENVASES = {"5L","10L","25L"};
     private static final float[] PRECIO_ACEITES = {(float)1.25,(float)5.0,(float)6.0,(float)7.0,(float)5.0};
     private static final float[] PRECIO_ENVASES = {1, 2, 3};
-
     
     private boolean regalar_envases;
     private float litros_aceite;
@@ -32,13 +18,39 @@ public class Venta {
     private float precio_total;
     
     /**
+     * Realiza una venta de envases
      * 
-     * @param tipo 
+     * @param tipo      Es el tipo del envase 1 - 5L, 2 - 10L, 3 - 25L
+     * @param cantidad  Es la cantidad de envases a vender
+     * 
+     * @return devuelve si se puede realizar la venta
      */
-    public void venderEnvase(int tipo, int cantidad){
-        num_envases[tipo] += cantidad;
+    public boolean venderEnvase(int tipo, int cantidad){
+        boolean ok = true;
         
-        if(!regalar_envases)
+        if(cantidad>0 && tipo<0 && tipo>2){
+            num_envases[tipo] += cantidad;
+        }else
+            ok = false;
+        
+        return ok;
     }
+    
+    public boolean venderAceite(){
+        boolean ok = true;
+        
+        
+        return ok;
+    }
+
+    public boolean isRegalar_envases() {
+        return regalar_envases;
+    }
+
+    public void setRegalar_envases(boolean regalar_envases) {
+        this.regalar_envases = regalar_envases;
+    }
+    
+    
     
 }
