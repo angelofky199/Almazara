2ef2883c38b642508f9af5d4d8a7f948b426697f
angelofky199 <angelofky199@hotmail.com>
2016-07-22 13:02:22 +0200

    gestor

--- AlmazaraBarcha/src/almazarabarcha/Modelo/GestorAlmazara.java
+++ AlmazaraBarcha/src/almazarabarcha/Modelo/GestorAlmazara.java
@@ -1,5 +1,19 @@
 package almazarabarcha.Modelo;
 
+import java.util.ArrayList;
+
 public class GestorAlmazara {
+    Cliente c = new Cliente(null, null, null, null, null, null);
+    ArrayList clientes;
+
+    public GestorAlmazara(Cliente c, ArrayList clientes) {
+        this.c = c;
+        this.clientes = clientes;
+    }
+    public void AnyadirClientes(Cliente c){
+        clientes = new ArrayList();
+        clientes.add(c);
+    }
+    
     
 }
