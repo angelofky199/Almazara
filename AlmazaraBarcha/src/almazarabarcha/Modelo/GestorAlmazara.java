cfc0254f79d12d2d6f0404228127ae1f657d4bdd
angelofky199 <angelofky199@hotmail.com>
2016-08-01 12:10:42 +0200

    a

--- AlmazaraBarcha/src/almazarabarcha/Modelo/GestorAlmazara.java
+++ AlmazaraBarcha/src/almazarabarcha/Modelo/GestorAlmazara.java
@@ -3,13 +3,12 @@ package almazarabarcha.Modelo;
 import java.util.ArrayList;
 
 public class GestorAlmazara {
-    Cliente cliente = new Cliente(null, null, null, null, null, null);
-    ArrayList clientes;
-    ArrayList compras;
-    ArrayList molturaciones;
+    Cliente cliente = new Cliente(null, null, null, null, null);
+    ArrayList<Cliente> clientes;
+
 
     public GestorAlmazara() {
-      
+   
     }
     public void AnyadirClientes(Cliente c){
         clientes = new ArrayList();
@@ -17,12 +16,17 @@ public class GestorAlmazara {
     }
     public void AnyadirMolturaciones(Molturacion molturacion){
         molturacion = new Molturacion();
-        molturaciones.add(molturacion);
+       // molturaciones.add(molturacion);
     }
     
     public void AnyadirCompras(Compra compra){
-        compras = new ArrayList();
-        compras.add(compra);
+        //compras = new ArrayList();
+        //compras.add(compra);
     }
-    
+    public String mostrar(){
+        
+       
+         return (clientes.get(0).getNombre_cliente());
+        
+    }
 }
