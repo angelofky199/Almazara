3f3abfe5614e86a919d90912e74194c852d862ec
Jose Ferrús <jose.ferrus.94@gmail.com>
2016-08-30 14:14:42 +0200

    Estilos

--- AlmazaraBarcha/src/almazarabarcha/estilos/Estilos.java
+++ AlmazaraBarcha/src/almazarabarcha/estilos/Estilos.java
@@ -1,5 +1,6 @@
 package almazarabarcha.estilos;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import java.net.URL;
@@ -25,6 +26,20 @@ public class Estilos{
     public Font getFuenteBotones(){
         CustomFont cf = new CustomFont();
         return cf.MyFont(0, (float)16);
+    }
+    
+    public Color getColorExterior(){
+        //return new Color(236, 254, 44);
+       // return new Color(255, 235, 59);
+        return new Color(255, 255, 0);
+    }
+    
+    public Color getColorInterior(){
+        return new Color(255, 255, 255);
+    }
+    
+    public Color getColorBotones(){
+        return new Color(251, 192, 45);
     }
     
     public Image getImagenIcono(){
